9b58c7f6c22cb41846196917564cbe3ea3e8110c
Giovani <giovani.aquino1612@gmail.com>
2025-05-18 19:48:14 -0300

    Java Mysql Conexão funcionando Crud e embelezamento faltando

--- src/VIEW/login_view.java
+++ src/VIEW/login_view.java
@@ -10,7 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class login_view extends JFrame {
-    private JTextField txt_senha_user;
+    private JPasswordField txt_senha_user;
     private JButton bt_entra;
     private JTextField txt_nome_user;
     private JPanel painel_login;
@@ -24,34 +24,37 @@ public class login_view extends JFrame {
        bt_entra.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    String nome_usuario, senha_usuario;
-
-                    nome_usuario = txt_nome_user.getText();
-                    senha_usuario = txt_senha_user.getText();
-
-                    user_dto obj_usuario_dto = new user_dto();
-                    obj_usuario_dto.setNome_usuario(nome_usuario);
-                    obj_usuario_dto.setSenha_usuario(senha_usuario);
-
-                    user_dao obj_userdao = new user_dao();
-                    ResultSet rs_userdao = obj_userdao.autenticacao_user(obj_usuario_dto);
-
-                    if (rs_userdao.next()){
-                        //chamar tela
-                        main_view obj_mainview = new main_view();
-                        obj_mainview.setVisible(true);
-                        dispose();
-                    }else {
-                        JOptionPane.showMessageDialog(null, "Usuario ou senha invalida");
-                    }
-                }catch (SQLException erro){
-                    JOptionPane.showMessageDialog(null, "loginView "+erro);
-                }
+                logar();
             }
         });
     }
     public static void main(String[] args){
         new login_view();
+    }
+    private void logar(){
+        try {
+            String nome_usuario, senha_usuario;
+
+            nome_usuario = txt_nome_user.getText();
+            senha_usuario = txt_senha_user.getText();
+
+            user_dto obj_usuario_dto = new user_dto();
+            obj_usuario_dto.setNome_usuario(nome_usuario);
+            obj_usuario_dto.setSenha_usuario(senha_usuario);
+
+            user_dao obj_userdao = new user_dao();
+            ResultSet rs_userdao = obj_userdao.autenticacao_user(obj_usuario_dto);
+
+            if (rs_userdao.next()){
+                //chamar tela
+                main_view obj_mainview = new main_view();
+                obj_mainview.setVisible(true);
+                dispose();
+            }else {
+                JOptionPane.showMessageDialog(null, "Usuario ou senha invalida");
+            }
+        }catch (SQLException erro){
+            JOptionPane.showMessageDialog(null, "loginView "+erro);
+        }
     }
 }
